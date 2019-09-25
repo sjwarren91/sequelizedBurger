75d092641685cedcf9ca323e39f9ac9da6b0afea
Sam Warren <sjwarren91@gmail.com>
2019-09-25 19:04:07 +1000

    sequelize setup

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 var express = require("express");
 var path = require("path");
 var hdb = require("express-handlebars");
-var routes = require("./controllers/burgers_controller")
-
+var db = require("./models");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.engine("handlebars", hdb({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(routes);
+require("./routes/burgers_controller")(app);
 
-app.listen(PORT, () => {
-    console.log("listening on port: " + PORT)
+db.sequelize.sync({}).then(() => {
+  app.listen(PORT, () => {
+    console.log("listening on port: " + PORT);
+  });
 });
