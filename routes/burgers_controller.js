5c7cff83f5cc14e0389af1f83daa6a8cada69a4d
Sam Warren <sjwarren91@gmail.com>
2019-09-25 19:33:05 +1000

    add update query

--- routes/burgers_controller.js
+++ routes/burgers_controller.js
@@ -1,47 +1,50 @@
 var db = require("../models");
 
-module.exports = (app) => {
-    app.get("/", (req, res) => {
-        db.Burger.findAll({}).then((data) => {
-            console.log(data);
-            var obj = [];
-            data.forEach(element => {
-                obj.push(element.dataValues)
-            });
+module.exports = app => {
+  app.get("/", (req, res) => {
+    db.Burger.findAll({}).then(data => {
+      var obj = [];
+      data.forEach(element => {
+        obj.push(element.dataValues);
+      });
 
-            console.log(obj);
-            res.render("index", {burgers: obj})
-        });
+      res.render("index", { burgers: obj });
     });
+  });
 
-    app.put("/api/burger/:id", (req, res) => {
+  app.put("/api/burger/:id", (req, res) => {
+    db.Burger.update(
+      {
+        devoured: req.body.devoured
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    ).then(data => {
+      if (data == 0) {
+        res.status(404);
+        return res.end();
+      } else {
+        res.status(200);
+        res.end();
+      }
+    });
+  });
 
-
-        // db.Burger.update(
-        //     ...req.body,
-        //     {
-        //     where: {
-        //         id: req.params.id
-        //     }
-        // }).then((data) => {
-
-        // })
-    })
-
-    app.post("/api/burger", (req, res) => {
-        console.log(req.body);
-        db.Burger.create(req.body).then((data) => {
-            res.status(200);
-            res.end();
-        })
-    })
-
-}
-
+  app.post("/api/burger", (req, res) => {
+    console.log(req.body);
+    db.Burger.create(req.body).then(data => {
+      res.status(200);
+      res.end();
+    });
+  });
+};
 
 // router.get("/", (req, res) => {
 //     burger.all((data) => {
-        
+
 //         console.log({burgers: data})
 
 //         res.render("index", {burgers: data})
@@ -73,7 +76,5 @@ module.exports = (app) => {
 //         res.json({id: result.insertId});
 //     })
 // })
-
-
 
 // module.exports = router;
